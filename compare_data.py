3db5095fae4b1d8a283f57beb1505b34490bd93a
Otto <sebastian.windeck@web.de>
2019-11-23 18:03:07 +0100

    update kanton_compare data
    - update compare dfs_kt function
    - reduce compare main to general functions

--- compare_data.py
+++ compare_data.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import logging
+import numpy as np
 from data_imports import *
 
 
-def compare_dfs(df_wikidata, df_statdata):
+def compare_dfs_kt(df_wikidata, df_statdata):
     """
-    This functions checks all entries from ZH Data and compares it with the
+    This functions checks all entries from ZH Kanton Data and compares it with the wikidata
     :param df_wikidata:
     :param df_statdata:
-    :return: list of index in df_statdata not in df_wikidata
+    :return: list of index in df_statdata not in df_wikidata --> need to be uploaded to wikidata
     """
     logging.info('Compare: {0} stat_entries with {1} wikidate_entries'.format(df_statdata['date'].count,
                                                                               df_wikidata['date'].count))
-    return df_statdata[df_statdata['date'].isin(df_wikidata['date']) == True].index.tolist()
 
+    # Create the check column
+    df_statdata['check'] = df_statdata['date'].astype(str) + '---' + df_statdata['BFS_NR'].astype(str)
+    df_wikidata['check'] = df_wikidata['date'].astype(str) + '---' + df_wikidata['bfs_id'].astype(str)
+    print("NOT IN THE DATASET")
+    print(df_statdata[(df_statdata['check'].isin(df_wikidata['check']) == False)]['date'].astype(str).str[:4].unique())
+
+    return df_statdata[(df_statdata['check'].isin(df_wikidata['check']) == False)].index.tolist()
+
+#TODO: for Gemeinde compare function
 
 def compare_kanton_mapping(df_wikidata, df_kanton):
 
@@ -25,31 +34,18 @@ def compare_kanton_mapping(df_wikidata, df_kanton):
 def main():
     # Database request of city/kanton level
     cityZH = import_cityZH_api()
-    print("City of Zurich")
-    #cityZH['date'] = cityZH['date'].dt.date
-    print(cityZH)
-
     kantonZH = import_kantonZH_api()
-    print("Kanton data")
-    print(kantonZH)
-    #kantonZH['date'] = kantonZH['date'].dt.date
+
     # Network requests
     geoadmin = import_geoadmin_wikidata_kt()
-    print("Kanton geo.admin.data")
-    print(geoadmin)
-    #geoadmin['date'] = geoadmin['date'].dt.date
     wikidataKT = import_wikidata_kt()
-    print("Wikidata for Kanton")
-    print(wikidataKT)
-    #wikidataKT['date'] = wikidataKT['date'].dt.date
 
-    # 1. Join Q_Numbers
     # wikidata_id
     print("MERGER")
-    merger = pd.merge(kantonZH, geoadmin, how='left', left_on=['BFS_NR'], right_on=['bfs'])
+    merger = pd.merge(kantonZH, geoadmin, how='left', left_on=['BFS_NR'], right_on=['bfs']).dropna()
     print(merger.head())
     # 2. Wiki mit API Kanton
-
+    print(compare_dfs_kt(wikidataKT, merger))
     # 2. Wiki
 
 
