2e0085a5c4a28d5c15ed3e0852d75db010a46577
Otto <sebastian.windeck@web.de>
2019-11-23 13:42:28 +0100

    Update compare_data.py
    Add logging and function description

--- compare_data.py
+++ compare_data.py
@@ -1,7 +1,14 @@
+import pandas as pd
+import logging
+
+
 def compare_dfs(df_wikidata, df_statdata):
-    '''
+    """
+    This functions checks all entries from ZH Data and compares it with the
     :param df_wikidata:
     :param df_statdata:
     :return: list of index in df_statdata not in df_wikidata
-    '''
+    """
+    logging.info('Compare: {0} stat_entries with {1} wikidate_entries'.format(df_statdata['date'].count,
+                                                                              df_wikidata['date'].count))
     return df_statdata[df_statdata['date'].isin(df_wikidata['date']) == True].index.tolist()
