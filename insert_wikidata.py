931ba0acfd1815f119b3277c61f3bc1b24577863
Otto <sebastian.windeck@web.de>
2019-11-24 15:57:44 +0100

    add bot for city data

--- insert_wikidata.py
+++ insert_wikidata.py
@@ -1,6 +1,17 @@
 # Quelle setzen
 #  Q4115189|P1082|805|P585|+2017-12-31T00:00:00Z/9|P459|Q12483|S854|"https://data.stadt-zuerich.ch/dataset/bev_bestand_jahr_quartier_seit1970_od3240"
 from wikidataintegrator import wdi_core, wdi_login
+from wikidataintegrator.ref_handlers import update_retrieved_if_new_multiple_refs
+import pandas as pd
+import numpy as np
+import requests
+from datetime import datetime
+from time import sleep
+from random import random
+
+# Quelle setzenx
+# Quartier
+#  Q4115189|P1082|805|P585|+2017-12-31T00:00:00Z/9|P459|Q12483|S854|"https://data.stadt-zuerich.ch/dataset/bev_bestand_jahr_quartier_seit1970_od3240"
 from datetime import datetime
 import copy
 def create_reference():
@@ -13,9 +24,9 @@ def create_reference():
 
     return [refRetrieved, refURL]
 
-def create_qualifier():
-    insertDate = datetime.now().strftime("+%Y-%m-%dT00:00:00Z")
+def create_qualifier(insertDate):
     # Point in time P585
+    insertDate = insertDate.strftime("+%Y-%m-%dT00:00:00Z")
     refRetrieved = wdi_core.WDTime(insertDate, prop_nr="P585", is_qualifier=True)
     # Statistics P459
     bestimmungsmethode = wdi_core.WDItemID(value='Q12483', prop_nr='P459', is_qualifier=True)
@@ -23,11 +34,18 @@ def create_qualifier():
     return [refRetrieved, bestimmungsmethode]
 
 
-def insert_wikidate():#
-    reference = create_reference()
-    qualfier = create_qualifier()
-    statement = [
-        wdi_core.WDQuantity(value=1001, prop_nr="P1082", references=[copy.deepcopy(reference)], rank='preferred',
-                            qualifiers=copy.deepcopy(qualfier), check_qualifier_equality=False)]
-    wd_item = wdi_core.WDItemEngine(data=statement, wd_item_id='Q4115189', new_item=False, fast_run=False)
-    wd_item.write(login)
+def insert_wikidate(df):#
+    df = df[df['bool'] == False]
+    for i, row in df.iterrows():
+        print("Insert data value: {0} - {1} - {2} : {3}".format(row['wikidata_id'], row['name'], row['date'],
+                                                                row['population']))
+        reference = create_reference()
+        insertDate = datetime(2017, 12, 31).strftime("+%Y-%m-%dT00:00:00Z")
+        qualfier = create_qualifier(row['date'])
+        statement = [
+            wdi_core.WDQuantity(value=row['population'], prop_nr="P1082", references=[copy.deepcopy(reference)],
+                                rank='normal', qualifiers=copy.deepcopy(qualfier), check_qualifier_equality=False)]
+        wd_item = wdi_core.WDItemEngine(data=statement, wd_item_id=row['wikidata_id'], new_item=False, fast_run=False,
+                                        append_value=['P1082'])
+        wd_item.write(login)
+        sleep(8 + random())
